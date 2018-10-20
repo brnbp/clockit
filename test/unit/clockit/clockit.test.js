11ac9a377b7ccce0c71a6c2b3950fdab87d16aae
brnbp <bruno9pereira@gmail.com>
2018-10-20 16:09:39 -0300

    wip

---
+++ test/unit/clockit/clockit.test.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+
+const { start, lunchIn, lunchOut, end, status, clearToday } = require('../../../src/clockit/clockit');
+
+describe('Clockit: start', () => {
+  it('Should start', () => {
+    
+  });
+});
